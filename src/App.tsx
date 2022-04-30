d60da01c57e965ac9e8c4b1f1b7dc2ab3f6b0645
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-04-30 22:54:37 +0300

    add redux the cycle, les 1

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import './App.css';
 function App() {
     return (
         <div className="container">
-            40 min
             <Lesson1 />
             {/*<Lesson3 />*/}
             {/*<Lesson4 />*/}
